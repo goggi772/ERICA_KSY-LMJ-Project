47e818114ecff1662df465929ffc51d1ecad3714
goggi772 <goggi772@gmail.com>
2019-12-03 00:48:21 +0900

    indianFoker setting file

--- setting.py
+++ setting.py
@@ -1,5 +1,4 @@
 import random
-import time
 def fresh_deck():
     #player_deck_input함수를 이용해 플레이어가 원하는 카드개수를 선택한다.(20장 or 40장)
     if player_deck_input() == 40:
@@ -11,11 +10,12 @@ def fresh_deck():
                 deck.append({"suit":i, "rank":k})
         random.shuffle(deck)
         print("*카드를 섞는 중입니다...")
+        import time
         time.sleep(1.1)
         present = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10]
         return deck, present
     else:
-        suit = ['♠','♣','♥','◆']
+        suits= ['♠','♣','♥','◆']
         ranks = ['A','A',2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
         deck = []
         for i in ranks:
@@ -28,8 +28,13 @@ def fresh_deck():
         return deck, present
 
 def player_deck_input():
-    player_input = int(input('*원하시는 카드의 개수를 입력해주세요. (20 또는 40) :'))
-    while not (player_input == 20 or player_input == 40):
-        print("*20 또는 40 으로 다시 입력해주세요.")
-        player_input = int(input('*원하시는 카드의 개수를 입력해주세요. (20 또는 40) :'))
-    return player_input
+    while True:
+        try:
+            player_input = int(input('*원하시는 카드의 개수를 입력해주세요. (20 또는 40) :'))
+            while not (player_input == 20 or player_input == 40):
+                print("*20 또는 40 으로 다시 입력해주세요.")
+                player_input = int(input('*원하시는 카드의 개수를 입력해주세요. (20 또는 40) :'))
+            return player_input
+        except ValueError:
+            print("*자연수로 입력해주세요.")
+            continue
