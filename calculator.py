79fa62d1f9cbfbae08dcce7eae30fa25b8ca160c
Lee-Minju <55554099+Lee-Minju@users.noreply.github.com>
2019-12-06 02:29:28 +0900

    length 수정

--- calculator.py
+++ calculator.py
@@ -1,10 +1,13 @@
 def select_behavior(present,opponentNum,minChips,maxChips):
     average = int(sum(present) / len(present)) #덱에 남은 카드들 평균
     weight  = abs(average - opponentNum)  #가중치
-    length = maxChips - minChips + 2 #다이의 경우의수포함
+    if minChips == 0:
+        length = (maxChips - minChips) + 1
+    else:
+        length = maxChips - minChips + 2 #다이의 경우의수포함
     p = 100 / length
     arr = []
-    for i in range(length):
+    for i in range(length): #다이부터 maxChip까지의 베팅 경우의수의 배열
         arr.append(p)
 
 
